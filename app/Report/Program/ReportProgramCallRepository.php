f51178629a3f10dae90667aff5380b12e8fa5536
Heinkl <hkhant96@gmail.com>
2017-08-24 15:27:28 +0630

    ReportRepository

---
+++ app/Report/Program/ReportProgramCallRepository.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: Mi Tin Zar Kyaw
+ * Date: 1/14/2017
+ * Time: 3:56 PM
+ */
+
+namespace App\Report\Program;
+
+use App\Backend\Country\Country;
+use App\Core\ReturnMessage;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Input;
+use App\Core\Utility;
+
+class ReportProgramCallRepository implements ReportProgramCallRepositoryInterface
+{
+    public function getProgramCall()
+    {
+        $program_calls = ProgramCall::whereNull('deleted_at')->get();
+        return $program_calls;
+    }
+
+    public function create($paramObj)
+    {
+        $returnedObj = array();
+        $returnedObj['aceplusStatusCode'] = ReturnMessage::INTERNAL_SERVER_ERROR;
+
+        try {
+            //$tempObj = Utility::addCreatedBy($paramObj);
+            $paramObj->save();
+
+            $returnedObj['aceplusStatusCode'] = ReturnMessage::OK;
+            return $returnedObj;
+        }
+        catch(\Exception $e){
+            $returnedObj['aceplusStatusMessage'] = $e->getMessage();
+            return $returnedObj;
+        }
+    }
+
+    public function update($paramObj)
+    {
+        $returnedObj = array();
+        $returnedObj['aceplusStatusCode'] = ReturnMessage::INTERNAL_SERVER_ERROR;
+
+        try {
+            $tempObj = Utility::addUpdatedBy($paramObj);
+            $tempObj->save();
+
+            $returnedObj['aceplusStatusCode'] = ReturnMessage::OK;
+            return $returnedObj;
+        }
+        catch(\Exception $e){
+
+            $returnedObj['aceplusStatusMessage'] = $e->getMessage();
+            return $returnedObj;
+        }
+    }
+
+    public function getObjByID($id){
+        $program_call = ProgramCall::find($id);
+        return $program_call;
+    }
+
+   public function delete($id){
+       try{
+           $tempObj = Accommodation::find($id);
+           $tempObj = Utility::addDeletedBy($tempObj);
+           $tempObj->deleted_at = date('Y-m-d H:m:i');
+           $tempObj->save();
+       }
+       catch(\Exception $e){
+       }
+    }
+
+    public function getPageIDByURL($url){
+        if($url == "/"){
+            $url = "home";
+        }
+
+        $page_id = DB::table('pages')->where('url',$url)->whereNull('deleted_at')->first()->id;
+        return $page_id;
+    }
+
+    public function getURLByPageID($page_id){
+        $url = DB::table('pages')->where('id',$page_id)->whereNull('deleted_at')->first()->url;
+        return $url;
+    }
+    
+    public function getCountry(){
+        $countries = DB::table('countries')->get();
+        return $countries;
+    }
+
+}
---
+++ resources/views/report/conference_reg_view.blade.php
@@ -0,0 +1,135 @@
+@extends('layouts.master')
+@section('title','Conference Registration')
+@section('content')
+    <div id="content" class="content">
+        <h1 class="page-header">Conference Registration Listing</h1>
+        @if(count(Session::get('message')) != 0)
+            <div>
+            </div>
+        @endif
+
+       {{-- <div class="row">
+            <div class="col-md-10"></div>
+            <div class="col-md-2">
+                <div class="buttons pull-right">
+                    <button type="button" onclick='create_setup("conference_registration");' class="btn btn-default btn-md first_btn">
+                        <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+                    </button>
+                    <button type="button" onclick='edit_setup("conference_registration");' class="btn btn-default btn-md second_btn">
+                        <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+                    </button>
+                    <button type="button" onclick="delete_setup('conference_registration');" class="btn btn-default btn-md third_btn">
+                        <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+                    </button>
+                </div>
+            </div>
+        </div>--}}
+        {!! Form::open(array('id'=> 'frm_conference_registration' ,'url' => 'backend/conference_registration/destroy', 'class'=> 'form-horizontal user-form-border')) !!}
+        {{ csrf_field() }}
+        <input type="hidden" id="selected_checkboxes" name="selected_checkboxes" value="">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="listing">
+                    <input type="hidden" id="pageSearchedValue" name="pageSearchedValue" value="">
+                    <table class="table table-striped list-table" id="list-table">
+                        <thead>
+                        <tr>
+                            <th><input type='checkbox' name='check' id='check_all'/></th>
+                          {{--  <th>First Name</th>
+                            <th>Middle Name</th>--}}
+                            <th>Last Name</th>
+                            <th>Organization</th>
+                            <th>Email</th>
+                            <th>Status</th>
+                            <th>Change Status</th>
+                        </tr>
+                        </thead>
+                        <tfoot>
+                        <tr>
+                            <th></th>
+                            {{--<th class="search-col" con-id="fname">First Name</th>
+                            <th class="search-col" con-id="mame">Middle Name</th>--}}
+                            <th class="search-col" con-id="lame">Last Name</th>
+                            <th class="search-col" con-id="organization">Organization</th>
+                            <th class="search-col" con-id="email">Email</th>
+                            <th class="search-col" con-id="status">Status</th>
+                            <th></th>
+                        </tr>
+                        </tfoot>
+                        <tbody>
+                        @foreach($conference_registrations as $conference_registration)
+                            <tr>
+                                <input type="hidden" name="id" value="{{$conference_registration->id}}">
+                                <td><input type="checkbox" class="check_source" name="edit_check" value="{{ $conference_registration->id }}" id="all"></td>
+                                {{--<td>{{$conference_registration->first_name}}</td>
+                                <td>{{$conference_registration->middle_name}}</td>--}}
+                                <td>{{$conference_registration->last_name}}</td>
+                                <td>{{$conference_registration->organization}}</td>
+                                <td>{{$conference_registration->email}}</td>
+                                <td>
+                                    @if($conference_registration->status == 1)
+                                        Pending
+                                    @elseif($conference_registration->status == 2)
+                                        Confirm
+                                    @elseif($conference_registration->status == 3)
+                                        Cancel
+                                    @endif
+                                </td>
+                                @if($conference_registration->status != 3 or $conference_registration->status != 2)
+                                <td>
+                                    <div class="btn btn-group-sm">
+                                        @if($conference_registration->status == 1)
+                                            <a class="btn btn-success" onclick="status_confirm_registration({{$conference_registration->id}})">Confirm</a><a onclick="status_cancel_registration({{$conference_registration->id}})" class="btn btn-danger">Cancel</a>
+                                        @elseif($conference_registration->status == 2)
+
+                                        @elseif($conference_registration->status == 3)
+
+                                        @endif
+                                    </div>
+                                </td>
+                                    @endif
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        {!! Form::close() !!}
+    </div>
+@stop
+@section('page_script')
+    <script type="text/javascript" language="javascript" class="init">
+        $(document).ready(function() {
+
+            $('#list-table tfoot th.search-col').each( function () {
+                var title = $('#list-table thead th').eq( $(this).index() ).text();
+                $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+            } );
+
+            var table = $('#list-table').DataTable({
+                aLengthMenu: [
+                    [5,25, 50, 100, 200, -1],
+                    [5,25, 50, 100, 200, "All"]
+                ],
+                iDisplayLength: 5,
+                "order": [[ 2, "desc" ]],
+                stateSave: false,
+                "pagingType": "full",
+                "dom": '<"pull-right m-t-20"i>rt<"bottom"lp><"clear">',
+
+            });
+
+            // Apply the search
+            table.columns().eq( 0 ).each( function ( colIdx ) {
+                $( 'input', table.column( colIdx ).footer() ).on( 'keyup change', function () {
+                    table
+                            .column( colIdx )
+                            .search( this.value )
+                            .draw();
+                } );
+
+            });
+        });
+    </script>
+@stop

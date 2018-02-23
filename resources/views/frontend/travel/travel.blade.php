8663a1ecc75dd76ef278507b6c246f39e11b35df
aungkokhant <aungkokhantace@gmail.com>
2018-02-23 13:43:22 +0630

    display post data in travel page

--- resources/views/frontend/travel/travel.blade.php
+++ resources/views/frontend/travel/travel.blade.php
@@ -6,19 +6,29 @@
         <div class="col-md-9 poster" id="poster">
              <h2 style="margin-bottom:15px;">TRAVEL & ACCOMMODATIONS >> POST-CONFERENCE TRAVEL (YANGON)</h2>
                 <div class="row">
+                  <div class="col-md-12">
+                    <!-- start displaying page and post data -->
+                    @if(isset($page->content) && $page->content !== "")
+                    {!! $page->content !!}
+                    <br>
+                    @endif
+                    @foreach($posts as $post)
+                        {!! $post->content !!}<br>
+                    @endforeach
+                    <!-- end displaying page and post data -->
+
                     @if(isset($postConferenceTravels) && count($postConferenceTravels)>0 )
                         @foreach($postConferenceTravels as $postConferenceTravel)
                         <!-- <div class="row"> -->
-                            <div class="col-md-12">
                                 <p><b>{!! $postConferenceTravel->name !!}</b></p>
                                 @if(isset($postConferenceTravel->image) && $postConferenceTravel->image !== null)
                                 <img src="{!! $postConferenceTravel->image !!}" class="img-responsive">
                                 @endif
                                 <p>{{$postConferenceTravel->description}}</p>
-                            </div>
                         <!-- </div> -->
                         @endforeach
                     @endif
+                  </div>
                 </div>
             </div>
         </div>
